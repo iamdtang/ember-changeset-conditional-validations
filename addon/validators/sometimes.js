72c3ddbd4c055a9f2e69f73b827163b1d2fce215
Rei <sovietspaceship@users.noreply.github.com>
2018-08-29 15:50:17 +0100

    support non-Array as argument to validateSometimes
    for consistency with plain ember-changeset-validations

--- addon/validators/sometimes.js
+++ addon/validators/sometimes.js
@@ -1,20 +1,22 @@
 import { get } from '@ember/object';
 
-export default function validateSometimes(validators, condition) {
-  return validators.map(function(validator) {
-    return function(key, newValue, oldValue, changes, content) {
-      let thisValue = {
-        get(property) {
-          return get(changes, property) != null
-            ? get(changes, property)
-            : get(content, property);
-        }
-      };
+export default function validateSometimes(validator, condition) {
+  return Array.isArray(validator) ? validator.map(mapValidator) : mapValidator(validator)
+}
 
-      if (condition.call(thisValue, changes, content)) {
-        return validator(key, newValue, oldValue, changes, content);
+function mapValidator(validator) {
+  return function(key, newValue, oldValue, changes, content) {
+    let thisValue = {
+      get(property) {
+        return get(changes, property) != null
+          ? get(changes, property)
+          : get(content, property);
       }
-      return true;
     };
-  });
+
+    if (condition.call(thisValue, changes, content)) {
+      return validator(key, newValue, oldValue, changes, content);
+    }
+    return true;
+  };
 }

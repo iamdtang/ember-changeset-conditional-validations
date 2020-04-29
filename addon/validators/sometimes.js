5f3ea7d345225dea86c7593e9b3a5a1443490b9f
Andy Pye <apye@sweettoothhq.com>
2020-04-29 09:58:26 +0300

    Fix .get() for sometimes condition with falsy values

--- addon/validators/sometimes.js
+++ addon/validators/sometimes.js
@@ -13,9 +13,31 @@ export default function validateSometimes(validator, condition) {
     return function(key, newValue, oldValue, changes, content) {
       let thisValue = {
         get(property) {
-          return get(changes, property) != null
-            ? get(changes, property)
-            : get(content, property);
+          if (property.includes('.')) {
+            let changesValue = get(changes, property);
+            if (typeof changesValue !== 'undefined') {
+              return changesValue;
+            }
+
+            // Check if the `changes` value is explicitly undefined,
+            // or if it's not present at all.
+            let pathSegments = property.split('.');
+            let propName = pathSegments.pop();
+            let objPath = pathSegments.join('.');
+
+            let obj = get(changes, objPath);
+            if (obj && obj.hasOwnProperty && obj.hasOwnProperty(propName)) {
+              return changesValue;
+            }
+
+            return get(content, property);
+          }
+
+          if (changes.hasOwnProperty(property)) {
+            return get(changes, property);
+          } else {
+            return get(content, property);
+          }
         }
       };
 
